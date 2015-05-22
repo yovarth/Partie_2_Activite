b97735741cd929c6ff2622fb76fcb1b22e7ba505
Yovarth <yovarth@gmail.com>
2015-05-22 18:39:09 +0200

    Ajout du fichier news.class.php

---
+++ news.class.php
@@ -0,0 +1,53 @@
+﻿<?php
+
+class quoideneuf
+{
+	private $_bdd; //Instance de PDO
+	
+	public function __construct($db)
+	{
+		$this->_bdd = $db;
+	}		
+	
+	public function getListNews()
+	{
+		$liste = array();
+		
+		$SQL = $this->_bdd->query('SELECT ID, date_news, news FROM news ORDER BY date_news DESC LIMIT 0,9');
+		while ($data = $SQL->fetch(PDO::FETCH_ASSOC))
+		{
+			$liste[] = $data;
+		}
+		$SQL->closeCursor();
+		return $liste;
+ 	}
+	
+	public function ajoutNews($dnews, $message)
+	{
+		$SQL = $this->_bdd->prepare('INSERT INTO news(date_news,news) VALUE (?, ?)');
+		$SQL->execute(array($dnews,$message));
+		if($SQL)
+		{
+			return True;
+		}
+		else
+		{
+			return False;
+		}
+		$SQL->closeCursor();		
+	}
+	
+	public function delNews($id)
+	{
+		$SQL = $this->_bdd->query('DELETE FROM news WHERE ID=' . $id);
+		if($SQL)
+		{
+			return True;
+		}
+		else
+		{
+			return False;
+		}
+		$SQL->closeCursor();	
+	}
+}
